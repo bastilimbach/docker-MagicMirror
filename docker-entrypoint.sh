c5645cca99c63b85698f86239de7ab921f58de81
Sebastian Limbach <bastilimbach@gmail.com>
2019-08-17 21:55:39 +0200

    Adapt entrypoint to check the folder content

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
+set -e
 
-if [ ! -f /opt/magic_mirror/modules ]; then
+if [ ! "$(ls -A /opt/magic_mirror/modules)" ]; then
     cp -Rn /opt/default_modules/. /opt/magic_mirror/modules
 fi
 
-if [ ! -f /opt/magic_mirror/config ]; then
-    cp -Rn /opt/default_config/. /opt/magic_mirror/config
+if [ ! "$(ls -A /opt/magic_mirror/config)" ]; then
+    cp /opt/magic_mirror/mm-docker-config.js /opt/magic_mirror/config/config.js
 fi
 
-exec $1
+exec "$@"
