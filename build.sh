5a04279ee06bf7ec03a6ddf1caa008261d137b86
Sebastian Limbach <bastilimbach@gmail.com>
2020-04-02 21:45:08 +0200

    Fix Travis

--- build.sh
+++ build.sh
@@ -2,7 +2,7 @@
 set -e
 
 # https://stackoverflow.com/a/51761312/4934537
-latest_release=$(git ls-remote --tags --refs --sort="v:refname" https://github.com/MichMich/MagicMirror.git | tail -n1 | sed 's/.*\///' | tr '\n' ' ')
+latest_release=$(git ls-remote --tags --refs --sort="v:refname" https://github.com/MichMich/MagicMirror.git | tail -n1 | sed 's/.*\///')
 if [ "$(docker manifest inspect bastilimbach/docker-magicmirror:"${latest_release}" > /dev/null; echo $?)" != 0 ]; then
   docker buildx build --progress plain --platform=linux/amd64,linux/arm64,linux/arm/v7 ${1} --build-arg branch="${latest_release}" -t "${DOCKER_USER}"/docker-magicmirror:"${latest_release}" -t "${DOCKER_USER}"/docker-magicmirror:latest .
 fi
