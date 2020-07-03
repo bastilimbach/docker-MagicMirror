5196b89ce7cd43d8a94e7d540ff71079f1971181
Leon Kiefer <leon.k97@gmx.de>
2020-07-03 22:39:20 +0200

    added config template feature

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -9,4 +9,8 @@ if [ ! "$(ls -A /opt/magic_mirror/config)" ]; then
     cp /opt/magic_mirror/mm-docker-config.js /opt/magic_mirror/config/config.js
 fi
 
+if [ -f "/opt/magic_mirror/config/config.js.template" ]; then
+    envsubst < /opt/magic_mirror/config/config.js.template > /opt/magic_mirror/config/config.js
+fi
+
 exec "$@"
